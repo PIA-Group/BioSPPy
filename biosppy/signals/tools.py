7f1b8f19be8ca4444d262cbd395c6a7c475d933e
Dominique Makowski <dom.mak19@gmail.com>
2016-11-13 14:45:16 +0100

    Update tools.py

--- biosppy/signals/tools.py
+++ biosppy/signals/tools.py
@@ -20,7 +20,25 @@ from scipy.stats import stats
 # local
 from .. import utils
 
-
+# Make it compatible with python
+import types
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+    
+    
+    
 def _norm_freq(frequency=None, sampling_rate=1000.):
     """Normalize frequency to Nyquist Frequency (Fs/2).
 
