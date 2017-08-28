4a4e7abe30bfd9069f79043d6df77a270c3183bb
Carlos Carreiras <capcarr@users.noreply.github.com>
2017-08-28 17:33:53 +0100

    Added a simple timing module.

---
+++ biosppy/timing.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+biosppy.timing
+--------------
+
+This module provides simple methods to measure computation times.
+
+:copyright: (c) 2015-2017 by Instituto de Telecomunicacoes
+:license: BSD 3-clause, see LICENSE for more details.
+"""
+
+# Imports
+# compat
+from __future__ import absolute_import, division, print_function
+# from six.moves import map, range, zip
+# import six
+
+# built-in
+import time
+
+# 3rd party
+
+# local
+
+# Globals
+CLOCKS = dict()
+DFC = '__default_clock__'
+
+
+def tic(name=None):
+    """Start the clock.
+    
+    Parameters
+    ----------
+    name : str, optional
+        Name of the clock; if None, uses the default name.
+    
+    """
+    
+    if name is None:
+        name = DFC
+    
+    CLOCKS[name] = time.time()
+
+
+def tac(name=None):
+    """Stop the clock.
+    
+    Parameters
+    ----------
+    name : str, optional
+        Name of the clock; if None, uses the default name.
+    
+    Returns
+    -------
+    delta : float
+        Elapsed time, in seconds.
+    
+    Raises
+    ------
+    KeyError if the name of the clock is unknown.
+    
+    """
+    
+    toc = time.time()
+    
+    if name is None:
+        name = DFC
+    
+    try:
+        delta = toc - CLOCKS[name]
+    except KeyError:
+        raise KeyError('Unknown clock.')
+    
+    return delta
+
+
+def clear(name=None):
+    """Clear the clock.
+    
+    Parameters
+    ----------
+    name : str, optional
+        Name of the clock; if None, uses the default name.
+    
+    """
+    
+    if name is None:
+        name = DFC
+    
+    CLOCKS.pop(name)
+
+
+def clear_all():
+    """Clear all clocks."""
+    
+    CLOCKS.clear()
