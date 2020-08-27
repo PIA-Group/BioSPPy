Fix onset calculation on BVP signals
- New onset detection function added (based on the work of Elgendi et al. 2013)
- BVP function now calls "find_onsets_elgendi2013" to calculate onsets.
- Change previous (and now deprecated) "find_onsets" function name to "find_onsets_zong03" to adequately refer to the method used.

biosppy/signals/bvp.py
@@ -68,7 +68,8 @@ def bvp(signal=None, sampling_rate=1000., show=True):
                                       sampling_rate=sampling_rate)
 
     # find onsets
-    onsets, = find_onsets(signal=filtered, sampling_rate=sampling_rate)
+    # onsets, = find_onsets(signal=filtered, sampling_rate=sampling_rate)
+    onsets, _ = find_onsets_elgendi2013(signal=filtered, sampling_rate=sampling_rate)
 
     # compute heart rate
     hr_idx, hr = st.get_heart_rate(beats=onsets,
@@ -99,10 +100,141 @@ def bvp(signal=None, sampling_rate=1000., show=True):
 
     return utils.ReturnTuple(args, names)
 
+def find_onsets_elgendi2013(signal=None, sampling_rate=1000., peakwindow=0.111, beatwindow=0.667, beatoffset=0.02, mindelay=0.3):
+    """
+    Determines onsets of BVP pulses.
 
-def find_onsets(signal=None, sampling_rate=1000., sm_size=None, size=None,
+    Parameters
+    ----------
+    signal : array
+        Input filtered BVP signal.
+    sampling_rate : int, float, optional
+        Sampling frequency (Hz).
+    peakwindow : float
+        Parameter W1 on referenced article
+        Optimized at 0.111
+    beatwindow : float
+        Parameter W2 on referenced article
+        Optimized at 0.667
+    beatoffset : float
+        Parameter beta on referenced article
+        Optimized at 0.2
+    mindelay : float
+        Minimum delay between peaks.
+        Avoids false positives
+
+    Returns
+    ----------
+    onsets : array
+        Indices of BVP pulse onsets.
+    params : dict
+        Input parameters of the function
+
+
+    References
+    ----------
+    - Elgendi M, Norton I, Brearley M, Abbott D, Schuurmans D (2013) Systolic Peak Detection in
+    Acceleration Photoplethysmograms Measured from Emergency Responders in Tropical Conditions.
+    PLoS ONE 8(10): e76585. doi:10.1371/journal.pone.0076585.
+    
+    Notes
+    ---------------------
+    Optimal ranges for signal filtering (from Elgendi et al. 2013):
+    "Optimization of the beat detector’s spectral window for the lower frequency resulted in a 
+    value within 0.5– 1 Hz with the higher frequency within 7–15 Hz"
+    
+    All the number references below between curly brackets {...} by the code refer to the line numbers of
+    code in "Table 2 Algorithm IV: DETECTOR (PPG signal, F1, F2, W1, W2, b)" from Elgendi et al. 2013 for a
+    better comparison of the algorithm
+    
+    """
+
+    # check inputs
+    if signal is None:
+        raise TypeError("Please specify an input signal.")
+
+    # Create copy of signal (not to modify the original object)
+    signal_copy = np.copy(signal)
+
+    # Truncate to zero and square
+    # {3, 4}
+    signal_copy[signal_copy < 0] = 0
+    squared_signal = signal_copy ** 2
+
+    # Calculate peak detection threshold
+    # {5}
+    ma_peak_kernel = int(np.rint(peakwindow * sampling_rate))
+    ma_peak, _ = st.smoother(squared_signal, kernel="boxcar", size=ma_peak_kernel)
+
+    # {6}
+    ma_beat_kernel = int(np.rint(beatwindow * sampling_rate))
+    ma_beat, _ = st.smoother(squared_signal, kernel="boxcar", size=ma_beat_kernel)
+
+    # Calculate threshold value
+    # {7, 8, 9}
+    thr1 = ma_beat + beatoffset * np.mean(squared_signal)
+
+    # Identify start and end of BVP waves.
+    # {10-16}
+    waves = ma_peak > thr1
+    beg_waves = np.where(np.logical_and(np.logical_not(waves[0:-1]), waves[1:]))[0]
+    end_waves = np.where(np.logical_and(waves[0:-1], np.logical_not(waves[1:])))[0]
+    # Throw out wave-ends that precede first wave-start.
+    end_waves = end_waves[end_waves > beg_waves[0]]
+
+    # Identify systolic peaks within waves (ignore waves that are too short).
+    num_waves = min(beg_waves.size, end_waves.size)
+    # {18}
+    min_len = int(np.rint(peakwindow * sampling_rate))
+    min_delay = int(np.rint(mindelay * sampling_rate))
+    onsets = [0]
+
+    # {19}
+    for i in range(num_waves):
+
+        beg = beg_waves[i]
+        end = end_waves[i]
+        len_wave = end - beg
+
+        # {20, 22, 23}
+        if len_wave < min_len:
+            continue
+
+        # Find local maxima and their prominence within wave span.
+        # {21}
+        data = signal_copy[beg:end]
+        locmax, props = scipy.signal.find_peaks(data, prominence=(None, None))
+
+        # If more than one peak
+        if locmax.size > 0:
+            # Identify most prominent local maximum.
+            peak = beg + locmax[np.argmax(props["prominences"])]
+            # Enforce minimum delay between onsets.
+            if peak - onsets[-1] > min_delay:
+                onsets.append(peak)
+
+    onsets.pop(0)
+    onsets = np.array(onsets, dtype='int')
+
+    # output
+    params = {'signal': signal, 'sampling_rate': sampling_rate, 'peakwindow': peakwindow, 'beatwindow': beatwindow, 'beatoffset': beatoffset, 'mindelay': mindelay}
+
+    args = (onsets, params)
+    names = ('onsets', 'params')
+
+    return utils.ReturnTuple(args, names)
+
+
+
+def find_onsets_zong03(signal=None, sampling_rate=1000., sm_size=None, size=None,
                 alpha=2., wrange=None, d1_th=0, d2_th=None):
-    """Determine onsets of BVP pulses.
+
+    """
+
+    DEPRECATED METHOD
+
+
+    Determine onsets of BVP pulses.
 
     Skips corrupted signal parts.
     Based on the approach by Zong *et al.* [Zong03]_.