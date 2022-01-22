8f56df7934c841235bb8327824787fe56690f43c
afonsof3rreira <afonsof3rreira@users.noreply.github.com>
2022-01-22 21:18:11 +0000

    Lazy loading enabled

--- biosppy/inter_plotting/__init__.py
+++ biosppy/inter_plotting/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+biosppy.signals
+---------------
+
+This package provides methods to interactively display plots for the
+following physiological signals (biosignals):
+    * Electrocardiogram (ECG)
+
+:copyright: (c) 2015-2021 by Instituto de Telecomunicacoes
+:license: BSD 3-clause, see LICENSE for more details.
+"""
+
+# compat
+from __future__ import absolute_import, division, print_function
+
+# allow lazy loading
+from . import ecg
